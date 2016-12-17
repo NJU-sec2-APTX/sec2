77018fa6a4b1cd9435c1e33f18ae94ee5d09b17e
一个大两个脑袋 <@172.26.45.200>
2016-12-18 00:49:23 +0800

    2016-12-18 实现了波本的要求

--- HotelReservationSystem-parent/HotelReservationSystem-Client/src/main/java/client/businessLogicServiceImpl/strategybl/PriceController.java
+++ HotelReservationSystem-parent/HotelReservationSystem-Client/src/main/java/client/businessLogicServiceImpl/strategybl/PriceController.java
@@ -2,7 +2,6 @@ package client.businessLogicServiceImpl.strategybl;
 
 import java.rmi.RemoteException;
 
-import client.Client;
 import client.businessLogicService.strategyblService.StrategyPriceService;
 import common.otherEnumClasses.StrategyList;
 import common.otherEnumClasses.UserRole;
@@ -12,7 +11,7 @@ public class PriceController implements StrategyPriceService{
 	private Strategy strategy;
 	
 	public StrategyList calPrice(OrderVO vo) throws RemoteException{
-		strategy=new Strategy(Client.getHotelDataService().getHotelInfo(vo.hotel).getAccountID(),UserRole.HotelWorker);
+		strategy=new Strategy(vo.clientId,UserRole.HotelWorker);
 		return strategy.calPrice(vo);
 	}
 }
